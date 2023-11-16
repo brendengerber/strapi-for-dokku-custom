5abc51b4b516024328c3d76dd286fb1e1b333da2
Brenden Gerber <10082638+brendengerber@users.noreply.github.com>
2023-11-16 17:15:20 -0600

    Add support for optional emails to be sent on backup failures

--- backup-to-s3.js
+++ backup-to-s3.js
@@ -1,12 +1,14 @@
-//To use this script set the environment variables AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY, and np
-//Optionally you can include a BACKUP_NOTIFICATION_EMAIL environment variable where backup logs will be sent if there is any errors
-//Note to receive email notification nodmailer logic is required in the catch block specific to your usecase
+//Script to backup strapi server to s3 and send email in case of error
+//See README.md for instructions to configure environment variables
 
 //Imports necessary modules
 const fs = require('fs');
 const path = require('path');
 const {execSync} = require('child_process');
 const {S3Client, PutObjectCommand} = require("@aws-sdk/client-s3");
+const nodemailer = require('nodemailer');
+require('dotenv').config();
+
 
 //Creates the s3Client to upload files
 const s3Client = new S3Client({region: "us-east-2", credentials: {accessKeyId: process.env.AWS_ACCESS_KEY_ID, secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY}});
@@ -22,6 +24,11 @@ try{
     //Finds the matching file names
     const fileNames = fs.readdirSync(directoryPath); 
     const matchingFileNames = fileNames.filter(file => path.extname(file) === extension);
+
+    //Throws an error if the strapi backup failed to complete
+    if(matchingFileNames.length < 1){
+        throw new Error('Strapi backup failed to complete!')
+    };
         
     for(let fileName of matchingFileNames){
             
@@ -36,8 +43,33 @@ try{
         fs.unlinkSync(path.join(__dirname, `/${fileName}`));   
     }
 
-}catch(err){
-    if(process.env.BACKUP_NOTIFICATION_EMAIL){
-        //Add nodemailer logic here if notification on failure is desired
+}catch(err){    
+    //Sends an email in case of failure if email credentials have been configured in environment variables
+    if(process.env.BACKUP_NOTIFICATION_SENDER_EMAIL){
+        const transporter = nodemailer.createTransport({
+            service: 'gmail',
+            auth: {
+                user: process.env.BACKUP_NOTIFICATION_SENDER_EMAIL,
+                pass: process.env.BACKUP_NOTIFICATION_EMAIL_SENDER_PASSWORD
+            }
+        });
+
+        let mailOptions = {
+            from: {
+                name: process.env.BACKUP_NOTIFICATION_SENDER_NAME || ' ',  
+                address: process.env.BACKUP_NOTIFICATION_ALTERNATE_SENDER_EMAIL || process.env.BACKUP_NOTIFICATION_SENDER_EMAIL
+            },
+            to: process.env.BACKUP_NOTIFICATION_DESTINATION_EMAIL,
+            subject: `Error backing up to ${process.env.AWS_BUCKET_NAME} S3 bucket`,
+            text: err.message
+        };
+
+        transporter.sendMail(mailOptions, function(error, info){
+            if (error) {
+                console.log("error here", error);
+            }else{
+                console.log('Email sent: ' + info.response);
+            }
+        });
     }
 }
