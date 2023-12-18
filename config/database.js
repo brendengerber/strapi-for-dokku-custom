d9a1e4c18f735082ebe6ec305bacc0542665af55
brendengerber <10082638+brendengerber@users.noreply.github.com>
2023-12-17 19:41:10 -0600

    Update database.js config file to allow for different databases depending on NODE_ENV variable

--- config/database.js
+++ config/database.js
@@ -1,5 +1,17 @@
 const path = require('path');
+require('dotenv').config();
 
+if(process.env.NODE_ENV === "development"){
+  module.exports = ({ env }) => ({
+    connection: {
+      client: 'sqlite',
+      connection: {
+        filename: path.join(__dirname, '..', env('DATABASE_FILENAME', '.tmp/data.db')),
+      },
+      useNullAsDefault: true,
+    },
+  });
+}else{
   module.exports = ({ env }) => ({
     connection: {
       client: 'postgres',
@@ -12,6 +24,5 @@ const path = require('path');
         ssl: env.bool('DATABASE_SSL', false),
       },
     },
-  });
-
-  
+  });  
+}
